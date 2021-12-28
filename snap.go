b166fadf89140313b7e86d71221f065f8a6af952
Ahmet Alp Balkan <ahmetalpbalkan@gmail.com>
2021-12-27 19:55:16 -0800

    offset coords based on cur display position

--- snap.go
+++ snap.go
@@ -5,19 +5,35 @@ import "github.com/gonutz/w32/v2"
 // TODO find a way to round up divisions consistently, otherwise we end up with off by 1px
 
 func toLeft(d w32.RECT, mul, div int32) w32.RECT {
-	return w32.RECT{Left: 0, Top: 0, Right: (d.Width() * mul) / div, Bottom: d.Height()}
+	return w32.RECT{
+		Left:   d.Left,
+		Top:    d.Top,
+		Right:  d.Left + (d.Width()*mul)/div,
+		Bottom: d.Top + d.Height()}
 }
 
 func toRight(d w32.RECT, mul, div int32) w32.RECT {
-	return w32.RECT{Left: d.Width() - d.Width()*mul/div, Top: 0, Right: d.Width(), Bottom: d.Height()}
+	return w32.RECT{
+		Left:   d.Left + d.Width() - d.Width()*mul/div,
+		Top:    d.Top,
+		Right:  d.Left + d.Width(),
+		Bottom: d.Height()}
 }
 
 func toTop(d w32.RECT, mul, div int32) w32.RECT {
-	return w32.RECT{Left: 0, Top: 0, Right: d.Width(), Bottom: d.Height() * mul / div}
+	return w32.RECT{
+		Left:   d.Left,
+		Top:    d.Top,
+		Right:  d.Left + d.Width(),
+		Bottom: d.Top + d.Height()*mul/div}
 }
 
 func toBottom(d w32.RECT, mul, div int32) w32.RECT {
-	return w32.RECT{Left: 0, Top: d.Height() - d.Height()*mul/div, Right: d.Width(), Bottom: d.Height()}
+	return w32.RECT{
+		Left:   d.Left,
+		Top:    d.Top + d.Height() - d.Height()*mul/div,
+		Right:  d.Left + d.Width(),
+		Bottom: d.Top + d.Height()}
 }
 
 func leftHalf(disp, _ w32.RECT) w32.RECT      { return toLeft(disp, 1, 2) }
